a36890b67b3654e61101f3a478968332d5fe0965
payce06 <paycegrossman@gmail.com>
2025-05-01 18:54:42 -0400

    index-html

---
+++ script.js
@@ -0,0 +1,30 @@
+const clock = document.getElementById('clock');
+const greeting = document.getElementById('gretting');
+
+function updateClock() {
+    const now = new Date();
+    const hours = now.getHours();
+    const mins = now.getMinutes();
+    const secs = now.getSeconds();
+
+    const formatted = [
+        hours.toString().padStart(2, '0'),
+        mins.toString().padStart(2, '0'),
+        secs.toString().padStart(2, '0')
+    ].join(':');
+
+    clock.textContent = formatted;
+
+    if (hours >= 5 && hours < 12) {
+        greeting.textContent = "Godd Morning 🌅";
+    } else if (hours >= 12 && hours < 17) {
+        greeting.textContent = "Good Afternoon ☀️";
+    } else if (hours >= 17 && hours < 21) {
+        greeting.textContent = "Good Evening 🌇";
+    } else {
+        greeting.textContent = "Good Night 🌙";
+    }
+}
+
+setInterval(updateClock, 1000);
+updateClock();
